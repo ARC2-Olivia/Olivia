a2ba2668dd95c485aee61af96c9c97a93eccefdc
DanijelFilipovic <dfilipovi@foi.hr>
2023-01-26 14:01:07 +0100

    Omogućeno dodjeljivanje odgovora na ponderirana pitanja.

--- src/Controller/EvaluationQuestionController.php
+++ src/Controller/EvaluationQuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\EvaluationQuestion;
+use App\Entity\EvaluationQuestionAnswer;
 use App\Form\EvaluationQuestionType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,7 +30,12 @@ class EvaluationQuestionController extends BaseController
             }
         }
 
-        return $this->render('evaluation/evaluation_question/edit.html.twig', ['evaluation' => $evaluationQuestion->getEvaluation(), 'form' => $form->createView(), 'activeCard' => 'editQuestion']);
+        return $this->render('evaluation/evaluation_question/edit.html.twig', [
+            'evaluationQuestion' => $evaluationQuestion,
+            'evaluation' => $evaluationQuestion->getEvaluation(),
+            'form' => $form->createView(),
+            'activeCard' => 'editQuestion'
+        ]);
     }
 
     #[Route("/delete/{evaluationQuestion}", name: "delete", methods: ["POST"])]
@@ -45,5 +51,46 @@ class EvaluationQuestionController extends BaseController
         }
 
         return $this->redirectToRoute('evaluation_evaluate', ['evaluation' => $evaluation->getId()]);
+    }
+
+    #[Route("/add-answers/{evaluationQuestion}", name: "add_answers", methods: ["POST"])]
+    #[IsGranted("ROLE_MODERATOR")]
+    public function addAnswers(EvaluationQuestion $evaluationQuestion, Request $request): Response
+    {
+        $csrfToken = $request->request->get('_csrf_token');
+        $answers = json_decode($request->request->get('answers'), true);
+
+        if ($csrfToken !== null && $this->isCsrfTokenValid('evaluationQuestion.addAnswers', $csrfToken)) {
+            if (count($answers) > 0) {
+                $this->clearExistingEvaluationQuestionAnswers($evaluationQuestion);
+                $this->createNewEvaluationQuestionAnswers($answers, $evaluationQuestion);
+                $this->em->flush();
+                $this->addFlash('success', $this->translator->trans('success.evaluationQuestionAnswer.add', [], 'message'));
+            } else {
+                $this->addFlash('error', $this->translator->trans('error.evaluationQuestionAnswer.empty', [], 'message'));
+            }
+        } else {
+            $this->addFlash('error', $this->translator->trans('error.evaluationQuestionAnswer.csrf', [], 'message'));
+        }
+
+        return $this->redirectToRoute('evaluation_question_edit', ['evaluationQuestion' => $evaluationQuestion->getId()]);
+    }
+
+    private function clearExistingEvaluationQuestionAnswers(EvaluationQuestion $evaluationQuestion): void
+    {
+        foreach ($evaluationQuestion->getEvaluationQuestionAnswers() as $eqa) {
+            $this->em->remove($eqa);
+        }
+    }
+
+    private function createNewEvaluationQuestionAnswers(mixed $answers, EvaluationQuestion $evaluationQuestion): void
+    {
+        foreach ($answers as $answer) {
+            $eqa = (new EvaluationQuestionAnswer())
+                ->setEvaluationQuestion($evaluationQuestion)
+                ->setAnswerText($answer['answerText'])
+                ->setAnswerValue($answer['answerValue']);
+            $this->em->persist($eqa);
+        }
     }
 }
