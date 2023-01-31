cf4fdb46462d014666aed85e6e40687211ebc6f6
DanijelFilipovic <dfilipovi@foi.hr>
2023-01-31 15:26:45 +0100

    Omogućeno uređivanje postavki specifično za evaluatore.

--- src/Controller/EvaluationEvaluatorController.php
+++ src/Controller/EvaluationEvaluatorController.php
@@ -36,7 +36,30 @@ class EvaluationEvaluatorController extends BaseController
         $evaluationEvaluatorImpl = $evaluationService->getEvaluatorImplementation($evaluationEvaluator);
         $baseForm = $this->createForm(EvaluationEvaluatorType::class, $evaluationEvaluator, ['edit_mode' => true]);
         $implForm = $this->createForm($evaluationService->getEvaluatorImplementationFormClass($evaluationEvaluator), $evaluationEvaluatorImpl);
+        $updated = false;
 
+        $baseForm->handleRequest($request);
+        if ($baseForm->isSubmitted() && $baseForm->isValid()) {
+            $this->em->flush();
+            $updated = true;
+        } else {
+            foreach ($baseForm->getErrors(true) as $error) {
+                $this->addFlash('error', $this->translator->trans($error->getMessage(), [], 'message'));
+            }
+        }
+
+        $implForm->handleRequest($request);
+        if ($implForm->isSubmitted() && $implForm->isValid()) {
+            if ($evaluationEvaluatorImpl->getId() === null) $this->em->persist($evaluationEvaluatorImpl);
+            $this->em->flush();
+            $updated = true;
+        } else {
+            foreach ($implForm->getErrors(true) as $error) {
+                $this->addFlash('error', $this->translator->trans($error->getMessage(), [], 'message'));
+            }
+        }
+
+        if ($updated === true) $this->addFlash('success', $this->translator->trans('success.evaluationEvaluator.edit', [], 'message'));
         return $this->render('evaluation/evaluation_evaluator/edit.html.twig', [
             'evaluation' => $evaluationEvaluator->getEvaluation(),
             'evaluationEvaluator' => $evaluationEvaluator,
