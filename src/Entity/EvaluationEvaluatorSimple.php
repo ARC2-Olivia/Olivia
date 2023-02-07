418e1583a6858d5b2a31be42bdab3595236bd641
DanijelFilipovic <dfilipovi@foi.hr>
2023-02-07 14:30:53 +0100

    Refaktoriranje

--- src/Entity/EvaluationEvaluatorSimple.php
+++ src/Entity/EvaluationEvaluatorSimple.php
@@ -37,23 +37,8 @@ class EvaluationEvaluatorSimple extends TranslatableEntity
     public function validate(ExecutionContextInterface $context, $payload): void
     {
         if ($this->evaluationEvaluator !== null && $this->evaluationEvaluator->isIncluded()) {
-            // Expected value and result text must not be NULL if the evaluator is included in the result generation.
-            if ($this->expectedValue === null) $context->buildViolation('error.evaluationEvaluatorSimple.expectedValue.blank')->atPath('expectedPath')->addViolation();
-            if ($this->resultText === null) $context->buildViolation('error.evaluationEvaluatorSimple.resultText')->atPath('resultText')->addViolation();
-
-            // Expected value has to be written in a specific way depending on the type of question. This will only be validated if the evaluator is included in the result
-            // generation.
-            if ($this->evaluationQuestion !== null) {
-                switch ($this->evaluationQuestion->getType()) {
-                    case EvaluationQuestion::TYPE_YES_NO:
-                        if ($this->expectedValue !== '0' && $this->expectedValue !== '1') $context->buildViolation('error.evaluationEvaluatorSimple.expectedValue.notBool')->atPath('expectedValue')->addViolation();
-                        break;
-                    case EvaluationQuestion::TYPE_WEIGHTED:
-                    case EvaluationQuestion::TYPE_NUMERICAL_INPUT:
-                        if (!is_numeric($this->expectedValue)) $context->buildViolation('error.evaluationEvaluatorSimple.expectedValue.notNumeric')->atPath('expectedValue')->addViolation();
-                        break;
-                }
-            }
+            $this->validateExpectedValue($context);
+            $this->validateResultText($context);
         }
     }
 
@@ -108,5 +93,31 @@ class EvaluationEvaluatorSimple extends TranslatableEntity
         $this->resultText = $resultText;
 
         return $this;
+    }
+
+    private function validateExpectedValue(ExecutionContextInterface $context): void
+    {
+        if ($this->expectedValue === null) {
+            $context->buildViolation('error.evaluationEvaluatorSimple.expectedValue.blank')->atPath('expectedPath')->addViolation();
+        }
+
+        if ($this->evaluationQuestion !== null) {
+            switch ($this->evaluationQuestion->getType()) {
+                case EvaluationQuestion::TYPE_YES_NO:
+                    if ($this->expectedValue !== '0' && $this->expectedValue !== '1') $context->buildViolation('error.evaluationEvaluatorSimple.expectedValue.notBool')->atPath('expectedValue')->addViolation();
+                    break;
+                case EvaluationQuestion::TYPE_WEIGHTED:
+                case EvaluationQuestion::TYPE_NUMERICAL_INPUT:
+                    if (!is_numeric($this->expectedValue)) $context->buildViolation('error.evaluationEvaluatorSimple.expectedValue.notNumeric')->atPath('expectedValue')->addViolation();
+                    break;
+            }
+        }
+    }
+
+    private function validateResultText(ExecutionContextInterface $context): void
+    {
+        if ($this->resultText === null) {
+            $context->buildViolation('error.evaluationEvaluatorSimple.resultText')->atPath('resultText')->addViolation();
+        }
     }
 }
