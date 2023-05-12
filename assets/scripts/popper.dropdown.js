9c85626669fff4701399ad0c24777903e5dbe5b3
DanijelFilipovic <dfilipovi@foi.hr>
2023-05-12 12:52:16 +0200

    Implementirani custom dropdown + Izmjene

---
+++ assets/scripts/popper.dropdown.js
@@ -0,0 +1,25 @@
+import { createPopper } from '@popperjs/core';
+import "../styles/dropdown.css";
+
+window.addEventListener('load', () => {
+    const dropdowns = document.querySelectorAll("[data-dropdown][data-for]");
+    for (const dropdown of dropdowns) {
+        const binding = document.querySelector(dropdown.dataset.for);
+        if (binding) {
+            createPopper(binding, dropdown, { placement: "bottom-end"});
+            binding.addEventListener("click", () => {
+               dropdown.setAttribute("data-show", "");
+            });
+            dropdown.style.minWidth = `${binding.offsetWidth}px`;
+        }
+    }
+
+    document.body.addEventListener("click", (e) => {
+        for (const dropdown of dropdowns) {
+            const binding = document.querySelector(dropdown.dataset.for);
+            if (dropdown.hasAttribute("data-show") && dropdown !== e.target && binding && binding !== e.target && !dropdown.contains(e.target)) {
+                dropdown.removeAttribute("data-show");
+            }
+        }
+    });
+});
