777ca232d6e7bf67436ba0093cefc592f7ca9fe9
DanijelFilipovic <dfilipovi@foi.hr>
2023-06-19 13:24:45 +0200

    Dodan import procesora

--- src/Misc/PSImporter.php
+++ src/Misc/PSImporter.php
@@ -4,6 +4,11 @@ namespace App\Misc;
 
 use App\Entity\PracticalSubmodule;
 use App\Entity\PracticalSubmodulePage;
+use App\Entity\PracticalSubmoduleProcessor;
+use App\Entity\PracticalSubmoduleProcessorProductAggregate;
+use App\Entity\PracticalSubmoduleProcessorSimple;
+use App\Entity\PracticalSubmoduleProcessorSumAggregate;
+use App\Entity\PracticalSubmoduleProcessorTemplatedText;
 use App\Entity\PracticalSubmoduleQuestion;
 use App\Entity\PracticalSubmoduleQuestionAnswer;
 use App\Exception\PSImport\ErroneousFirstTaskException;
@@ -20,6 +25,7 @@ class PSImporter
 
     private int $taskIndex = 1;
     private array $questionMapping = [];
+    private array $processorMapping = [];
 
     public function __construct(array $tasks, EntityManagerInterface $em)
     {
@@ -50,10 +56,13 @@ class PSImporter
             }
 
             switch ($task['task_op']) {
-                case Tasks::CREATE_SUBMODULE: $this->doCreateSubmoduleTask($task['task_props']); break;
-                case Tasks::CREATE_QUESTION: $this->doCreateQuestionTask($task['task_props']); break;
-                case Tasks::CREATE_QUESTION_DEPENDENCY: $this->doCreateQuestionDependencyTask($task['task_props']); break;
-                case Tasks::CREATE_PAGE: $this->doCreatePageTask($task['task_props']); break;
+                case Tasks::CREATE_SUBMODULE:                       $this->doCreateSubmoduleTask($task['task_props']);                    break;
+                case Tasks::CREATE_QUESTION:                        $this->doCreateQuestionTask($task['task_props']);                     break;
+                case Tasks::BIND_QUESTION_DEPENDENCY:               $this->doBindQuestionDependencyTask($task['task_props']);             break;
+                case Tasks::CREATE_PROCESSOR:                       $this->doCreateProcessorTask($task['task_props']);                    break;
+                case Tasks::BIND_PROCESSOR_DEPENDENCY_ON_QUESTION:  $this->doBindProcessorDependencyOnQuestionTask($task['task_props']);  break;
+                case Tasks::BIND_PROCESSOR_DEPENDENCY_ON_PROCESSOR: $this->doBindProcessorDependencyOnProcessorTask($task['task_props']); break;
+                case Tasks::CREATE_PAGE:                            $this->doCreatePageTask($task['task_props']);                         break;
             }
 
             $this->taskIndex++;
@@ -136,7 +145,7 @@ class PSImporter
         }
     }
 
-    private function doCreateQuestionDependencyTask(array $props): void
+    private function doBindQuestionDependencyTask(array $props): void
     {
         if (false === $this->allKeysExist(['question', 'dependent', 'value'], $props)) {
             return;
@@ -151,6 +160,141 @@ class PSImporter
         $dependent = $this->questionMapping[$props['dependent']];
         $question->setDependentPracticalSubmoduleQuestion($dependent)->setDependentValue($props['value']);
         $this->em->persist($question);
+    }
+
+    private function doCreateProcessorTask(array $props): void
+    {
+        if (false === $this->allKeysExist(['id', 'type', 'name', 'included', 'position', 'impl'], $props)) {
+            return;
+        }
+
+        $processor = (new PracticalSubmoduleProcessor())
+            ->setType($props['type'])
+            ->setName($props['name'])
+            ->setIncluded($props['included'])
+            ->setPosition($props['position'])
+        ;
+
+        $this->em->persist($processor);
+        $this->processorMapping[$props['id']] = $processor;
+
+        if (true === empty($props['impl'])) {
+            return;
+        }
+
+        $implProps = $props['impl'];
+        switch ($processor->getType()) {
+            case $processor::TYPE_SIMPLE: {
+                if (false === $this->allKeysExist(['expectedValue', 'resultText'], $implProps)) return;
+                $pspSimple = (new PracticalSubmoduleProcessorSimple())
+                    ->setExpectedValue($implProps['expectedValue'])
+                    ->setResultText($implProps['resultText'])
+                ;
+                $processor->setPracticalSubmoduleProcessorSimple($pspSimple);
+                $this->em->persist($pspSimple);
+            } break;
+            case $processor::TYPE_SUM_AGGREGATE: {
+                if (false === $this->allKeysExist(['expectedValueRangeStart', 'expectedValueRangeEnd', 'resultText'], $implProps)) return;
+                $pspSumAggregate = (new PracticalSubmoduleProcessorSumAggregate())
+                    ->setExpectedValueRangeStart($implProps['expectedValueRangeStart'])
+                    ->setExpectedValueRangeEnd($implProps['expectedValueRangeEnd'])
+                    ->setResultText($implProps['resultText'])
+                ;
+                $processor->setPracticalSubmoduleProcessorSumAggregate($pspSumAggregate);
+                $this->em->persist($pspSumAggregate);
+            } break;
+            case $processor::TYPE_PRODUCT_AGGREGATE: {
+                if (false === $this->allKeysExist(['expectedValueRangeStart', 'expectedValueRangeEnd', 'resultText'], $implProps)) return;
+                $pspProductAggregate = (new PracticalSubmoduleProcessorProductAggregate())
+                    ->setExpectedValueRangeStart($implProps['expectedValueRangeStart'])
+                    ->setExpectedValueRangeEnd($implProps['expectedValueRangeEnd'])
+                    ->setResultText($implProps['resultText'])
+                ;
+                $processor->setPracticalSubmoduleProcessorProductAggregate($pspProductAggregate);
+                $this->em->persist($pspProductAggregate);
+            } break;
+            case $processor::TYPE_TEMPLATED_TEXT: {
+                if (false === key_exists('resultText', $implProps)) return;
+                $pspTemplatedText = (new PracticalSubmoduleProcessorTemplatedText())->setResultText($implProps['resultText']);
+                $processor->setPracticalSubmoduleProcessorTemplatedText($pspTemplatedText);
+                $this->em->persist($pspTemplatedText);
+            } break;
+        }
+    }
+
+    private function doBindProcessorDependencyOnQuestionTask(array $props): void
+    {
+        if (false === $this->allKeysExist(['processor', 'dependent'], $props)) {
+            return;
+        }
+
+        $processorId = $props['processor'];
+        $questionId = $props['dependent'];
+
+        if (false === (key_exists($processorId, $this->processorMapping) && key_exists($questionId, $this->questionMapping))) {
+            return;
+        }
+
+        /** @var PracticalSubmoduleProcessor $processor */
+        $processor = $this->processorMapping[$processorId];
+        $question = $this->questionMapping[$questionId];
+
+        switch ($processor->getType()) {
+            case $processor::TYPE_SIMPLE: {
+                $pspSimple = $processor->getPracticalSubmoduleProcessorSimple();
+                $pspSimple->setPracticalSubmoduleQuestion($question);
+                $this->em->persist($pspSimple);
+            } break;
+            case $processor::TYPE_SUM_AGGREGATE: {
+                $pspSumAggregate = $processor->getPracticalSubmoduleProcessorSumAggregate();
+                $pspSumAggregate->addPracticalSubmoduleQuestion($question);
+                $this->em->persist($pspSumAggregate);
+            } break;
+            case $processor::TYPE_PRODUCT_AGGREGATE: {
+                $pspProductAggregate = $processor->getPracticalSubmoduleProcessorProductAggregate();
+                $pspProductAggregate->addPracticalSubmoduleQuestion($question);
+                $this->em->persist($pspProductAggregate);
+            } break;
+            case $processor::TYPE_TEMPLATED_TEXT: {
+                $pspTemplatedText = $processor->getPracticalSubmoduleProcessorTemplatedText();
+                $pspTemplatedText->setPracticalSubmoduleQuestion($question);
+                $this->em->persist($pspTemplatedText);
+            } break;
+        }
+    }
+
+    private function doBindProcessorDependencyOnProcessorTask(array $props): void
+    {
+        if (false === $this->allKeysExist(['processor', 'dependent'], $props)) {
+            return;
+        }
+
+        $processorId = $props['processor'];
+        $otherProcessorId = $props['dependent'];
+
+        if (false === $this->allKeysExist([$processorId, $otherProcessorId], $this->processorMapping)) {
+            return;
+        }
+
+        /** @var PracticalSubmoduleProcessor $processor */
+        $processor = $this->processorMapping[$processorId];
+        if (false === in_array($processor->getType(), $processor::getProcessorProcessingProcessorTypes())) {
+            return;
+        }
+
+        $otherProcessor = $this->processorMapping[$otherProcessorId];
+        switch ($processor->getType()) {
+            case $processor::TYPE_SUM_AGGREGATE: {
+                $pspSumAggregate = $processor->getPracticalSubmoduleProcessorSumAggregate();
+                $pspSumAggregate->addPracticalSubmoduleProcessor($otherProcessor);
+                $this->em->persist($pspSumAggregate);
+            } break;
+            case $processor::TYPE_PRODUCT_AGGREGATE: {
+                $pspProductAggregate = $processor->getPracticalSubmoduleProcessorProductAggregate();
+                $pspProductAggregate->addPracticalSubmoduleProcessor($otherProcessor);
+                $this->em->persist($pspProductAggregate);
+            } break;
+        }
     }
 
     private function doCreatePageTask(array $props): void
