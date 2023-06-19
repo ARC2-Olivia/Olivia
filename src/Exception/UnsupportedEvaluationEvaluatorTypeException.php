4f39d8db07eb3b6aa2eeb3750f0e7aa4b3ae1299
DanijelFilipovic <dfilipovi@foi.hr>
2023-06-19 09:40:57 +0200

    Izmjena naziva pozvane funkcije

--- src/Exception/UnsupportedEvaluationEvaluatorTypeException.php
+++ src/Exception/UnsupportedEvaluationEvaluatorTypeException.php
@@ -8,7 +8,7 @@ class UnsupportedEvaluationEvaluatorTypeException extends \Exception
 {
     public static function withDefaultMessage(int $code = 0, ?\Throwable $previous = null): self
     {
-        $message = sprintf('The following evaluation evaluator types are currently supported: %s.', implode(', ', PracticalSubmoduleProcessor::getSupportedEvaluationEvaluatorTypes()));
+        $message = sprintf('The following evaluation evaluator types are currently supported: %s.', implode(', ', PracticalSubmoduleProcessor::getSupportedProcessorTypes()));
         return new UnsupportedEvaluationEvaluatorTypeException($message, $code, $previous);
     }
 }
