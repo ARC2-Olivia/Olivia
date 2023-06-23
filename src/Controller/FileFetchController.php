a40991a7450e1900e7adb74804dc34fe213fa510
DanijelFilipovic <dfilipovi@foi.hr>
2023-06-23 10:36:57 +0200

    Omogućeno preuzimanje datoteke.

--- src/Controller/FileFetchController.php
+++ src/Controller/FileFetchController.php
@@ -3,17 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Course;
+use App\Entity\File;
 use App\Entity\Instructor;
 use App\Entity\LessonItemFile;
 use App\Entity\PracticalSubmodule;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/file-fetch", name: "file_fetch_")]
 class FileFetchController extends AbstractController
 {
     #[Route("/course-image/{course}", name: 'course_image')]
-    public function courseImage(Course $course)
+    public function courseImage(Course $course): Response
     {
         $dir = $this->getParameter('dir.course_image');
         $filepath = $dir . '/' . $course->getImage();
@@ -21,7 +23,7 @@ class FileFetchController extends AbstractController
     }
 
     #[Route("/practical-submodule-image/{practicalSubmodule}", name: 'practical_submodule_image')]
-    public function practicalSubmoduleImage(PracticalSubmodule $practicalSubmodule)
+    public function practicalSubmoduleImage(PracticalSubmodule $practicalSubmodule): Response
     {
         $dir = $this->getParameter('dir.practical_submodule_image');
         $filepath = $dir . '/' . $practicalSubmodule->getImage();
@@ -29,7 +31,7 @@ class FileFetchController extends AbstractController
     }
 
     #[Route("/instructor-image/{instructor}", name: "instructor_image")]
-    public function instructorImage(Instructor $instructor)
+    public function instructorImage(Instructor $instructor): Response
     {
         $dir = $this->getParameter('dir.instructor_image');
         $filepath = $dir . '/' . $instructor->getImage();
@@ -37,10 +39,16 @@ class FileFetchController extends AbstractController
     }
 
     #[Route("/lesson-file/{lessonItemFile}", name: 'lesson_file')]
-    public function lessonFile(LessonItemFile $lessonItemFile)
+    public function lessonFile(LessonItemFile $lessonItemFile): Response
     {
         $dir = $this->getParameter('dir.lesson_file');
         $filepath = $dir . '/' . $lessonItemFile->getFilename();
         return $this->file($filepath);
+    }
+
+    #[Route("/uploaded-file/{file}", name: "uploaded_file")]
+    public function uploadedFile(File $file): Response
+    {
+        return $this->file($file->getPath(), $file->getOriginalName());
     }
 }
