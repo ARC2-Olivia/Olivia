9770d8008fc7da37949a0df7fc9333cc5dbf5a65
DanijelFilipovic <dfilipovi@foi.hr>
2023-09-18 14:01:24 +0200

    Bazični prikaz obrasca za kreiranje nove tematike.

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Topic;
+use App\Form\TopicType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/topic", name: "topic_")]
+#[Route("/{_locale}/topic", name: "topic_", requirements: ["_locale" => "%locale.supported%"])]
 class TopicController extends BaseController
 {
     #[Route("/", name: "index")]
@@ -14,5 +17,20 @@ class TopicController extends BaseController
     {
         $topics = $this->em->getRepository(Topic::class)->findAll();
         return $this->render('topic/index.html.twig', ['topics' => $topics]);
+    }
+
+    #[Route("/new", name: "new")]
+    #[IsGranted("ROLE_MODERATOR")]
+    public function new(Request $request): Response
+    {
+        $topic = new Topic();
+        $form = $this->createForm(TopicType::class, $topic, ['include_translatable_fields' => true]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
+
+        return $this->render('topic/new.html.twig', ['form' => $form->createView()]);
     }
 }
--- src/Form/TopicType.php
+++ src/Form/TopicType.php
@@ -7,23 +7,32 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TopicType extends AbstractType
 {
+    private ?TranslatorInterface $translator = null;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title', TextType::class, [
                 'label' => 'form.entity.topic.label.title',
-                'attr' => ['class' => 'form-input mb-3']
+                'attr' => ['class' => 'form-input mb-3', 'placeholder' => $this->translator->trans('form.entity.topic.placeholder.title', domain: 'app')]
             ])
         ;
 
         if (true === $options['include_translatable_fields']) {
             $builder
-                ->add('title', TextType::class, [
-                    'label' => 'form.entity.topic.label.title',
-                    'attr' => ['class' => 'form-input mb-3']
+                ->add('titleAlt', TextType::class, [
+                    'mapped' => false,
+                    'label' => 'form.entity.topic.label.titleAlt',
+                    'attr' => ['class' => 'form-input mb-3', 'placeholder' => $this->translator->trans('form.entity.topic.placeholder.title', domain: 'app')]
                 ])
             ;
         }
