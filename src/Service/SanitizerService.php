1fee8b389926f027e1783a272d43e0b85e23e447
DanijelFilipovic <dfilipovi@foi.hr>
2023-10-13 10:36:08 +0200

    Dodatni dopušteni tag-ovi

--- src/Service/SanitizerService.php
+++ src/Service/SanitizerService.php
@@ -25,7 +25,7 @@ class SanitizerService
     public function sanitizeHtml(?string $string): ?string
     {
         if (null !== $string) {
-            $allowed = ['p', 'a', 'img', 'br', 'span', 'ul', 'ol', 'li', 'b', 'i', 'u', 's', 'em', 'strong'];
+            $allowed = ['p', 'a', 'img', 'br', 'span', 'ul', 'ol', 'li', 'b', 'i', 'u', 's', 'em', 'strong', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
             $string = strip_tags($string, $allowed);
         }
 
