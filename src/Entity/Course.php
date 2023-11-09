9f1554a001985ba3a883ce1b5cbf16f155cf597d
DanijelFilipovic <dfilipovi@foi.hr>
2023-11-09 14:01:31 +0100

    Dorađeni toast-evi.

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -81,7 +81,7 @@ class Course extends TranslatableEntity
         return $this->name;
     }
 
-    public function setName(string $name): self
+    public function setName(?string $name): self
     {
         $this->name = $name;
 
