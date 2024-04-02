fc73957dfe6c6e4656d7f213bd76e29f47e140f0
DanijelFilipovic <dfilipovi@foi.hr>
2024-04-02 13:24:40 +0200

    Dodana stranica za javne datoteke + uređena funkcionalnost oko toga

--- src/Form/BasicFileUploadType.php
+++ src/Form/BasicFileUploadType.php
@@ -2,19 +2,29 @@
 
 namespace App\Form;
 
+use App\Entity\File;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class BasicFileUploadType extends AbstractType
 {
+    private ?TranslatorInterface $translator = null;
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $fileOptions = [
             'label' => 'common.file',
             'attr' => ['class' => 'form-input mb-3'],
+            'required' => $options['requiredFile']
         ];
 
         if (null !== $options['mimeTypes']) {
@@ -28,6 +38,10 @@ class BasicFileUploadType extends AbstractType
         }
 
         $builder->add('file', FileType::class, $fileOptions);
+        if (true === $options['selectType']) {
+            $typeChoices = [$this->translator->trans('file.type.file', [], 'app') => File::TYPE_FILE, $this->translator->trans('file.type.video', [], 'app') => File::TYPE_VIDEO];
+            $builder->add('type', ChoiceType::class, ['choices' => $typeChoices, 'attr' => ['class' => 'form-select'], 'label' => 'form.entity.file.label.type']);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -36,7 +50,9 @@ class BasicFileUploadType extends AbstractType
             'translation_domain' => 'app',
             'attr' => ['class' => 'd-flex flex-column'],
             'mimeTypes' => null,
-            'extensions' => null
+            'extensions' => null,
+            'selectType' => false,
+            'requiredFile' => true
         ]);
     }
 }
