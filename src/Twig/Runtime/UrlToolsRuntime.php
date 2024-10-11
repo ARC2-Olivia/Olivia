84098371e41796a662018636c2513796c6b47e72
DanijelFilipovic <dfilipovi@foi.hr>
2024-10-11 14:07:35 +0200

    http:\\ -> https:\\

--- src/Twig/Runtime/UrlToolsRuntime.php
+++ src/Twig/Runtime/UrlToolsRuntime.php
@@ -4,10 +4,15 @@ namespace App\Twig\Runtime;
 
 use Twig\Extension\RuntimeExtensionInterface;
 
-class UrlDecodeRuntime implements RuntimeExtensionInterface
+class UrlToolsRuntime implements RuntimeExtensionInterface
 {
     public function urlDecode(string $url): string
     {
         return urldecode($url);
+    }
+
+    public function toHttps(string $url): string
+    {
+        return str_replace('http://', 'https://', $url);
     }
 }
