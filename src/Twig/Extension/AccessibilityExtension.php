150cf18a4e3457715bdde2a3f2425ab180c910ae
DanijelFilipovic <dfilipovi@foi.hr>
2025-02-13 15:31:28 +0100

    Rad na kontrastnom modu
    (cherry picked from commit bad6142b2f2609bb0df5d2b1d87bb4ac87456126)

--- src/Twig/Extension/AccessibilityExtension.php
+++ src/Twig/Extension/AccessibilityExtension.php
@@ -11,7 +11,9 @@ class AccessibilityExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('is_dylexia_mode_enabled', [AccessibilityRuntime::class, 'isDyslexiaModeEnabled'])
+            new TwigFunction('is_dyslexia_mode_enabled', [AccessibilityRuntime::class, 'isDyslexiaModeEnabled']),
+            new TwigFunction('is_contrast_mode_enabled', [AccessibilityRuntime::class, 'isContrastModeEnabled']),
+            new TwigFunction('accessibility_body_class', [AccessibilityRuntime::class, 'getAccessibilityBodyClass'])
         ];
     }
 }
